17c533eeb39f090a5922b3009a679ce499bdd864
Lobzter <olowo15tomiloba@gmail.com>
2024-07-30 15:13:49 +0100

    stage1

---
+++ src/components/Bussines.jsx
@@ -0,0 +1,8 @@
+
+import React from 'react'
+
+export const Bussines = () => {
+  return (
+    <div>Bussines</div>
+  )
+}
---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import { NavBar } from "./NavBar";
+import { Billing } from "./Billing";
+import { Bussines } from "./Bussines";
+import Button from "./Button";
+import { CardDeal } from "./CardDeal";
+import { Clients } from "./Clients";
+import { CTA } from "./CTA";
+import { FeedBack } from "./FeedBack";
+import { Footer } from "./Footer";
+import { GetStarted } from "./GetStarted";
+import { Hero } from "./Hero";
+import { Stats } from "./Stats";
+import { Testimonials } from "./Testimonials";
+
+export {
+    NavBar,
+    Billing,
+    Bussines,
+    Button,
+    CardDeal,
+    Clients,
+    CTA,
+    FeedBack,
+    Footer,
+    GetStarted,
+    Hero,
+    Stats,
+    Testimonials
+}
